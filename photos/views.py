261db14fa3ce989e6115691dbc36c5cd316b1817
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-04 23:55:29 +0300

    add photo views

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Photo
 # Create your views here.
+
+
+def photo_list(request):
+    queryset = Photo.object.all()
+    context = {
+        "photos": queryset,
+    }
+    return render(request, "photo.html", context)
