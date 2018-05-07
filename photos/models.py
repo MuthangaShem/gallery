da9b7096c65849f62311449db021ec588869d957
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-05-07 11:27:09 +0300

    Add image class

--- photos/models.py
+++ photos/models.py
@@ -38,3 +38,23 @@ class Category(models.Model):
     @classmethod
     def update_image(cls, id, value):
         cls.objects.filter(id=id).update(category_name=value)
+
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=20)
+    image_description = models.TextField(max_length=200)
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('published at', auto_now_add=True)
+    image_url = models.ImageField(upload_to='images/%Y-%m-%d', null=True)
+
+    def __str__(self):
+        return self.image_name
+
+    # save method
+    def image_save(self):
+        self.save()
+    # delete method
+
+    def image_delete(self):
+        self.delete()
